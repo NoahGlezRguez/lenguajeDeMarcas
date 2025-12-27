60a3f151fb436846256cd4d07686bb1f246eb482
Noah González Rodríguez <noah.gonzalezro@elorrieta-errekamari.com>
2025-12-27 22:18:03 +0100

    facilito

---
+++ Entrega_UD4_ej05_IF/ej5_2.js
@@ -0,0 +1,11 @@
+
+let num1 = prompt("Escribe aquí un nº: ");
+
+let num2 = prompt("Escribe otro también: ");
+
+if (num1 < num2)
+	alert("El segundo número es mayor");
+else if (num1 > num2)
+	alert("El primer número es mayor");
+else if (num1 == num2)
+	alert("Los números son iguales");
