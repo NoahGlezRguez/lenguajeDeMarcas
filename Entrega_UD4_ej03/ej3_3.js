20dc3f0d2602addfe0d05c20385d5669693692ae
Noah González Rodríguez <noah.gonzalezro@elorrieta-errekamari.com>
2025-12-27 21:54:20 +0100

    intento 1 a ver si funciona

--- Entrega_UD4_ej03/ej3_3.js
+++ Entrega_UD4_ej03/ej3_3.js
@@ -4,4 +4,22 @@ let ane = "Ane", oi = "Oihan", jo = "Jon", zu = "Zuri", ma = "Maria", ant = "Ant
 let clasificaciones = [ane, oi, jo, zu, ma, ant];
 
 alert("Clasificación actual:\n1º " + clasificaciones[0] + "\n2º " + clasificaciones[1] + "\n3º " + clasificaciones[2] 
-		+ "\n4º " + clasificaciones[3] + "\n5º " + clasificaciones[4] + "\n6º " + clasificaciones[5] + "\n7º " + clasificaciones[6]);
+		+ "\n4º " + clasificaciones[3] + "\n5º " + clasificaciones[4] + "\n6º " + clasificaciones[5]);
+
+let tmp = clasificaciones.indexOf(zu);
+
+clasificaciones[clasificaciones.indexOf(jon)] = zu;
+clasificaciones[tmp] = jon;
+
+tmp = clasificaciones.indexOf(ant);
+
+clasificaciones.splice(tmp, 1);
+
+tmp = clasificaciones.indexOf(ane) + 1;
+clasificaciones.splice(tmp, 0, "Eneko", "Amaia");
+
+clasificaciones.splice(0, 0, "Mariana");
+
+alert("Clasificación actual:\n1º " + clasificaciones[0] + "\n2º " + clasificaciones[1] + "\n3º " + clasificaciones[2] 
+		+ "\n4º " + clasificaciones[3] + "\n5º " + clasificaciones[4] + "\n6º " + clasificaciones[5] + "\n7º " + clasificaciones[6]
+		+ "\n8º " + clasificaciones[7] + "\n9º " + clasificaciones[8]);
