c7ec928671016e9d6bc20d9b3512dccc9ff37a5f
Noah González Rodríguez <noah.gonzalezro@elorrieta-errekamari.com>
2025-12-27 22:20:55 +0100

    apartado 3 ok

---
+++ Entrega_UD4_ej05_IF/ej5_3.js
@@ -0,0 +1,9 @@
+
+let num1 = prompt("Escribe aquí un nº: ");
+
+let num2 = prompt("Escribe otro también: ");
+
+if (num1 > 10 && num2 > 10)
+	alert("Los dos números son más grandes que 10");
+else 
+	alert("Los dos números NO son más grandes que 10");
