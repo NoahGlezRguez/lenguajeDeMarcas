97de9c6cecd445e0aca8a36321eaa38df612cf5a
Noah González Rodríguez <noah.gonzalezro@elorrieta-errekamari.com>
2025-12-27 21:05:22 +0100

    ajuste del alert segun enunciado

--- Entrega_UD4_ej03/ej3_02.js
+++ Entrega_UD4_ej03/ej3_02.js
@@ -5,6 +5,4 @@ nombre[0] = prompt("Escribe un primer nombre:");
 nombre[1] = prompt("Escribe un segundo nombre:");
 nombre[2] = prompt("Escribe un tercer nombre:");
 
-for(let i = 0; i < nombre.length; i++){
-	alert(nombre[i]);
-}
+alert("los nombres son: " + nombre[0] + ", " + nombre[1] + " y " + nombre[2] + ".");
