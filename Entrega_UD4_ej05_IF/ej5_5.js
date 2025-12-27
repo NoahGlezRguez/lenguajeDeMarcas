e17c12b6b8c74dc8317d923724a961b5a401cd31
Noah González Rodríguez <noah.gonzalezro@elorrieta-errekamari.com>
2025-12-27 23:36:10 +0100

    ej5_5
    si no se parsea a int el resultado de la operacion con math, nunca se acertara

---
+++ Entrega_UD4_ej05_IF/ej5_5.js
@@ -0,0 +1,10 @@
+let num1 = parseInt(Math.random(10, 1 + 1) * 10);
+
+let num2 = prompt("Escribe un nº entre el 1 y el 10: ");
+
+if (num1 == num2)
+	alert("¡Has acertado!");
+else if (num1 > num2)
+	alert("El número que has introducido es menor.");
+else
+	alert("El número que has introducido es mayor.");
