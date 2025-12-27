f0e651142b9b26ccc110fe2b1b2d4712f831cb15
Noah González Rodríguez <noah.gonzalezro@elorrieta-errekamari.com>
2025-12-27 20:59:11 +0100

    ej 3_02

--- Entrega_UD4_ej03/ej3_02.js
+++ Entrega_UD4_ej03/ej3_02.js
@@ -4,3 +4,7 @@ let nombre = [];
 nombre[0] = prompt("Escribe un primer nombre:");
 nombre[1] = prompt("Escribe un segundo nombre:");
 nombre[2] = prompt("Escribe un tercer nombre:");
+
+for(let i = 0; i < nombre.length; i++){
+	alert(nombre[i]);
+}
