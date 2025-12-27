9ac84d72192ab2a35c65af8e60d187a68b00c679
Noah González Rodríguez <noah.gonzalezro@elorrieta-errekamari.com>
2025-12-27 21:22:55 +0100

    matriz clasificaciones inicial

---
+++ Entrega_UD4_ej03/ej3_3.js
@@ -0,0 +1,7 @@
+
+let ane = "Ane", oi = "Oihan", jo = "Jon", zu = "Zuri", ma = "Maria", ant = "Antton"
+
+let clasificaciones = [ane, oi, jo, zu, ma, ant];
+
+alert("Clasificación actual:\n1º " + clasificaciones[0] + "\n2º " + clasificaciones[1] + "\n3º " + clasificaciones[2] 
+		+ "\n4º " + clasificaciones[3] + "\n5º " + clasificaciones[4] + "\n6º " + clasificaciones[5] + "\n7º " + clasificaciones[6]);
