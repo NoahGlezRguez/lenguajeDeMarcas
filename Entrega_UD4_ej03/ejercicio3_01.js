52f02151d41d9c795ebca7c85da1be92d9e924ed
Noah González Rodríguez <noah.gonzalezro@elorrieta-errekamari.com>
2025-12-27 20:49:33 +0100

    empezando el ej3

---
+++ Entrega_UD4_ej03/ejercicio3_01.js
@@ -0,0 +1,7 @@
+
+
+let meses = ["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"];
+
+for (let i = 0; i < meses.length; i++){
+	alert(meses[i]);
+}
