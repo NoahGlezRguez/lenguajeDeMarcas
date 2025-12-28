3a6f8065182c3386f2bdad29c924a105dd59101f
Noah González Rodríguez <noah.gonzalezro@elorrieta-errekamari.com>
2025-12-28 22:40:40 +0100

    fix: revisar despues Y antes tambien + opcion todas se repiten

--- Entrega_UD4_ej07_Funciones/ej7_07.js
+++ Entrega_UD4_ej07_Funciones/ej7_07.js
@@ -2,27 +2,27 @@
 
 function primerCharNoRep(texto){
 
-	var letra;
-	var	primeraVez = true;
 	var numReps;
 
 	for (let i = 0; i < texto.length; i++){
 		numReps = 0;
-		for(let j = i + 1; j < texto.length; j++){
+		for(let j = 0; j < texto.length; j++){
 			if (texto.charAt(i) == texto.charAt(j))
 				numReps++;
 		}
 		
-		if (numReps == 0 && primeraVez == true){
-			letra = texto.charAt(i);
-			primeraVez = false;
-		}
+		if (numReps == 1)
+			return (texto.charAt(i));
+		
 	}
-	return (letra);
+	return (null);
 }
 
 let texto = prompt("Escribe un texto aquí:");
 
 var letra = primerCharNoRep(texto);
 
-alert("La primera letra que no se repite en el texto que has introducido es la " + letra);
+if (letra != null)
+	alert("La primera letra que no se repite en el texto que has introducido es la " + letra);
+else
+	alert("Todas las letras se repiten.");
