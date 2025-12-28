44e8bf45660c0da236882dd12a14f9bbd3e576da
Noah González Rodríguez <noah.gonzalezro@elorrieta-errekamari.com>
2025-12-28 17:46:30 +0100

    2 terminado

--- Entrega_UD4_ej07_Funciones/ej7_02.js
+++ Entrega_UD4_ej07_Funciones/ej7_02.js
@@ -4,12 +4,17 @@ function detectaCase(texto){
 	
 	var tieneMayus = false;
 	var tieneMinus = false;
-	
-	for(let i = 0; i < texto.lenght; i++){
-		if (texto.charAt(i) >= 65 && texto.charAt(i) <= 90)
+
+	for(let i = 0; i < texto.length; i++){
+		/*	charAt no sirve aqui porque charAt de javascript devuelve una letra (string de un caracter de largo)
+			mientra que java devuelve un char, que es un número del código ascii
+		*/
+
+		if (texto.charCodeAt(i) >= 65 && texto.charCodeAt(i) <= 90)
 			tieneMayus = true;
-		else if (texto.charAt(i) >= 97 && texto.charAt(i) <= 122)
+		else if (texto.charCodeAt(i) >= 97 && texto.charCodeAt(i) <= 122)
 			tieneMinus = true;
+
 	}
 	if (tieneMayus && tieneMinus)
 		alert("El texto tiene mayúsculas y minúsculas.");
