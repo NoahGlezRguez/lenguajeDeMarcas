dfb27a21ff329371404bda766c7e24a12f9f6dee
Noah González Rodríguez <noah.gonzalezro@elorrieta-errekamari.com>
2025-12-28 19:22:04 +0100

    comentario con explicacion

--- Entrega_UD4_ej07_Funciones/ej7_05.js
+++ Entrega_UD4_ej07_Funciones/ej7_05.js
@@ -30,3 +30,28 @@ for(let i = 0; i < combos.length - 1; i++)
 resultado = resultado + combos[combos.length - 1] + ".";
 
 alert("El resultado es: " + resultado);
+
+/*
+He tenido que analizar un ejemplo con una palabra, habia elegido "texto".
+
+Con las llamadas recursivas, cada iteracion del for llamaria a la funcion recursiva,
+e iria por niveles. Cada letra, por separado (t, e, x, t, o) a parte de ser una combinacion posible
+como tal, genera otro nivel (llama a la funcion recursiva).
+
+en ese nivel, cada posible combinacion (te, tx, tt, to) genera otro nivel recursivo.
+
+tex y tet tambien generan otro nivel.
+
+cuando el numero de combinaciones se reduce, no realiza mas llamadas porque no se cumple la condicion del
+bucle (se llega al fin de la palabra y no se puede iterar mas).
+
+gracias a que se guarda el fragmento acumulado (t, te, tex, text) y el indice (desde donde empieza a mirar con i),
+no se repiten combinaciones ni se hace combinaciones con las letras en orden inverso o repetidas.
+
+realmente, primero se consigue todas las combinaciones que empiezan por la primera letra, luego con la siguiente y asi...
+
+como se guarda en un array y con javascript se adapta el tamaño del array, esto ayuda tambien.
+
+luego simplemente los concateno con formato en un string y lo saco con un alert.
+
+*/
