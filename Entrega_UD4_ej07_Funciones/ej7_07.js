b3bb2162c93a9c6e303c44eada5a0225a74cea10
Noah González Rodríguez <noah.gonzalezro@elorrieta-errekamari.com>
2025-12-28 22:36:40 +0100

    intento 2

--- Entrega_UD4_ej07_Funciones/ej7_07.js
+++ Entrega_UD4_ej07_Funciones/ej7_07.js
@@ -2,22 +2,27 @@
 
 function primerCharNoRep(texto){
 
-	var indice;
-
+	var letra;
+	var	primeraVez = true;
 	var numReps;
 
 	for (let i = 0; i < texto.length; i++){
 		numReps = 0;
-		for(let j = i + 1; j < texto.length - i; j++){
+		for(let j = i + 1; j < texto.length; j++){
 			if (texto.charAt(i) == texto.charAt(j))
 				numReps++;
 		}
-		if (numReps == 0)
-			indice = texto.charAt(i);
+		
+		if (numReps == 0 && primeraVez == true){
+			letra = texto.charAt(i);
+			primeraVez = false;
+		}
 	}
-
+	return (letra);
 }
 
 let texto = prompt("Escribe un texto aquí:");
 
-primerCharNoRep(texto);
+var letra = primerCharNoRep(texto);
+
+alert("La primera letra que no se repite en el texto que has introducido es la " + letra);
